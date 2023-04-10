d0f9fb1484289888698c89c0f5c271f2556fba12
aaron <aaron-dario.fries@stud.fh-campuswien.ac.at>
2023-04-10 20:06:25 +0200

    Fix Imports and the MovieAPI

--- src/main/java/at/ac/fhcampuswien/fhmdb/api/User.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/api/User.java
@@ -1,4 +1,11 @@
 package at.ac.fhcampuswien.fhmdb.api;
 
 public class User {
+    public double getAge() {
+        return 0;
+    }
+
+    public String getUsername() {
+        return null;
+    }
 }
