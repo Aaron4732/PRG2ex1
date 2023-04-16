6893c1bd7bb977833b88d8725a89df33a453d78f
aaron <aaron-dario.fries@stud.fh-campuswien.ac.at>
2023-04-16 18:13:09 +0200

    Create MovieAPI

--- src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPI.java
@@ -1,36 +1,42 @@
 package at.ac.fhcampuswien.fhmdb.api;
+import at.ac.fhcampuswien.fhmdb.models.Movie;
 import com.google.gson.Gson;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 import java.io.IOException;
 import java.util.Arrays;
 
 public class MovieAPI {
-    private static final OkHttpClient client = new OkHttpClient();
+    OkHttpClient client = new OkHttpClient();
+    Gson gson = new Gson();
 
-    String run(String url) throws IOException {
+    String jsonString;
+
+    public Movie[] MovieAPI(String url) throws IOException {
+        this.run(url);
+        return movieParser();
+     }
+
+    public void run(String url) throws IOException {
+
         Request request = new Request.Builder()
                 .url(url)
+                .header("User-Agent","Firefox/112.0")
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
+            ResponseBody responseBody = response.body();
+
+            if (responseBody != null) {
+                jsonString = responseBody.string();
+            }
         }
     }
 
-    public static <T> void main(String[] args) throws IOException {
-        CheckAPI example = new CheckAPI();
-        String response = null;
-
-        response = example.run("http://localhost:3002/users");
-
-        System.out.println(response);
-
-        Gson gson = new Gson();
-        User[] users = gson.fromJson(response, User[].class);
-
-        System.out.println(Arrays.toString(users));
+    public Movie[] movieParser() {
+        return gson.fromJson(jsonString, Movie[].class);
     }
 }
